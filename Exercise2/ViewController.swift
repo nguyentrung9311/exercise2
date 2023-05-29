7ce4a9e43ef21a9263c6067291a341c461867fe0
TrungNV (Macbook) <trungnv@ecomobile.vn>
2023-05-29 21:34:19 +0700

    Init Project

---
+++ Exercise2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exercise2
+//
+//  Created by TrungNV (Macbook) on 29/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
